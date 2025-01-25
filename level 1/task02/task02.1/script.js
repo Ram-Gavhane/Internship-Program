9705a943659aea79481ce590eb3c04e712281c6c
Ram Gavhane <ramgavhane28@gmail.com>
2025-01-25 15:24:32 +0530

    Completed task02.1

---
+++ level 1/task02/task02.1/script.js
@@ -0,0 +1,9 @@
+function generateRGBColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+function changeclr(){
+    document.getElementById("btn").style.backgroundColor = generateRGBColor();
+}
